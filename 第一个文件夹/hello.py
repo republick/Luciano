9c233b756fb0956a52afe5fc6e79fd50f0b69b7c
luciano <zw_luciano@163.com>
2022-03-30 20:19:35 +0800

    第一个文件夹/hello.py

---
+++ 第一个文件夹/hello.py
@@ -0,0 +1,9 @@
+'''
+Author: your name
+Date: 2022-03-30 20:17:34
+LastEditTime: 2022-03-30 20:17:35
+LastEditors: Please set LastEditors
+Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+FilePath: \Luciano\第一个文件夹\hello.py
+'''
+print("hello world")
